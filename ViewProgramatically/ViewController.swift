6cdf269cc05a2a7387883517c359524293cbdc53
Eric Fuentes <ericfuentes@erics-macbook-pro.local>
2019-01-15 12:55:18 -0500

    Initial Commit

---
+++ ViewProgramatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewProgramatically
+//
+//  Created by Eric Fuentes on 1/15/19.
+//  Copyright © 2019 Eric Fuentes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
